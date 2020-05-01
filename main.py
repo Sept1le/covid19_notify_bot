a77eeb48715334a9a8f136eef62fd2645d45a527
TriangulumGalaxy <59769865+TriangulumGalaxy@users.noreply.github.com>
2020-05-01 21:12:54 +0300

    Fixes, declension of words

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ from discord.ext import commands
 
 import asyncio
 
-from config import TOKEN, message_about_covid
+from config import TOKEN, message_about_covid, help_message
+
+import pymorphy2
+
+# count_in_russia = 'test'
+# count_in_world = 'test'
+# count_in_region = 2
 
 class covidBot(commands.Cog):
+    morph = pymorphy2.MorphAnalyzer()
+    human = morph.parse('человек')[0]
+
     def __init__(self, bot):
         self.bot = bot
 
@@ -15,17 +24,21 @@ class covidBot(commands.Cog):
     async def on_message(self, message):
         pass
 
-    @commands.command(name='about')
+    @commands.command(name='about', brief='информация о коронавирусе')
     async def about(self, ctx):
         await ctx.send(message_about_covid)
 
-    @commands.command(name='stats')
+    @commands.command(name='stats', brief='Показывает общую статистику')
     async def stats(self, ctx):
-        await ctx.send(f'Заражено в 🇷🇺 России: {count_in_russia}\nЗаражено в 🗺️ мире: {count_in_world} чел.👥')
+        await ctx.send(f'Заражено в 🇷🇺 России: {count_in_russia} чел.👥\nЗаражено в 🗺️ мире: {count_in_world} чел.👥')
+        await ctx.send('test')
 
-    @commands.command(name='change_region')
-    async def change_region(self, ctx, region_name):
-        await ctx.send(f'В {region_name} заражено {count_in_region} чел.👥')
+    @commands.command(name='region_stats', brief='"название области" - информация о регионе')
+    async def change_region(self, ctx, region_first_name, region_second_name):
+        if str(count_in_region).endswith('1'):
+            await ctx.send(f'В {region_first_name} {region_second_name} заражен {count_in_region} {self.human.make_agree_with_number(count_in_region).word}.👥')
+        else:
+            await ctx.send(f'В "{region_first_name} {region_second_name}" заражены {count_in_region} {self.human.make_agree_with_number(count_in_region).word}.👥')
 
 
 bot = commands.Bot(command_prefix='!')
