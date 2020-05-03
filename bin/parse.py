2d42017c320c94424301bf5c54d4ef3859f16f13
s3pt1l3 <45712837+Sept1le@users.noreply.github.com>
2020-05-03 23:46:32 +0300

    Added some methods
    World statistics method added, Russia statistics method added

--- bin/parse.py
+++ bin/parse.py
@@ -34,6 +34,13 @@ class Parser:
                 self.russian_dic[city][0] = self.russian_dic[city][0][:self.russian_dic[city][0].find('+')]
             else:
                 pass
+            if "-" in self.russian_dic[city][0]:
+                self.russian_dic[city][1] = self.russian_dic[city][0][self.russian_dic[city][0].find('-') + 1:]
+                self.russian_dic[city][0] = self.russian_dic[city][0][:self.russian_dic[city][0].find('-')]
+            else:
+                pass
+        for key in self.russian_dic:
+            self.regions.append(key)
 
     def get_world(self):
         items = self.soup.find_all("div", class_='row border border-bottom-0 c_search2_row')
@@ -51,56 +58,82 @@ class Parser:
                 self.world_dic[country][1] = self.world_dic[country][0][self.world_dic[country][0].find('+') + 1:]
                 self.world_dic[country][0] = self.world_dic[country][0][:self.world_dic[country][0].find('+')]
             else:
-                pass
+                if "-" in self.world_dic[country][0]:
+                    self.world_dic[country][1] = self.world_dic[country][0][self.world_dic[country][0].find('-') + 1:]
+                    self.world_dic[country][0] = self.world_dic[country][0][:self.world_dic[country][0].find('-')]
+                else:
+                    pass
+        for key in self.world_dic:
+            self.countries.append(key)
 
     def region_all(self, region):
-        self.get_russian_regions()
         return self.russian_dic[region][0]
 
     def region_new(self, region):
-        self.get_russian_regions()
         return self.russian_dic[region][1]
 
     def region_sick(self, region):
-        self.get_russian_regions()
         return self.russian_dic[region][2]
 
     def region_healthy(self, region):
-        self.get_russian_regions()
         return self.russian_dic[region][3]
 
     def region_dead(self, region):
-        self.get_russian_regions()
         return self.russian_dic[region][4]
 
     def world_all(self, country):
-        self.get_world()
         return self.world_dic[country][0]
 
     def world_new(self, country):
-        self.get_world()
         return self.world_dic[country][1]
 
     def world_sick(self, country):
-        self.get_world()
         return self.world_dic[country][2]
 
     def world_healthy(self, country):
-        self.get_world()
         return self.world_dic[country][3]
 
     def world_dead(self, country):
-        self.get_world()
         return self.world_dic[country][4]
 
-    def world_list(self):
+    def world_stats(self):
         self.get_world()
-        for key in self.world_dic:
-            self.countries.append(key)
-        return self.countries
+        all = 0
+        today = 0
+        sick = 0
+        healthy = 0
+        dead = 0
+        stats = []
+        for i in self.countries:
+            all += int(self.world_dic[i][0])
+            today += int(self.world_dic[i][1])
+            sick += int(self.world_dic[i][2])
+            healthy += int(self.world_dic[i][3])
+            dead += int(self.world_dic[i][4])
+        stats.append(all)
+        stats.append(today)
+        stats.append(sick)
+        stats.append(healthy)
+        stats.append(dead)
+        return stats
 
-    def region_list(self):
+    def rus_stats(self):
         self.get_russian_regions()
-        for key in self.russian_dic:
-            self.regions.append(key)
-        return self.regions
+        all = 0
+        today = 0
+        sick = 0
+        healthy = 0
+        dead = 0
+        stats = []
+        for i in self.regions:
+            all += int(self.russian_dic[i][0])
+            today += int(self.russian_dic[i][1])
+            sick += int(self.russian_dic[i][2])
+            healthy += int(self.russian_dic[i][3])
+            dead += int(self.russian_dic[i][4])
+        stats.append(all)
+        stats.append(today)
+        stats.append(sick)
+        stats.append(healthy)
+        stats.append(dead)
+        return stats
